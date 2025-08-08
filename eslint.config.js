be548c4ff184c14a81763eaacb5abf9c5f1824c0
Shubham Palav <shubham.palav@cbsindia.co.in>
2025-08-08 15:35:03 +0530

    changes

--- eslint.config.js
+++ eslint.config.js
@@ -2,12 +2,20 @@ import js from "@eslint/js";
 import nextPlugin from "@next/eslint-plugin-next";
 
 export default [
+  // Base JS recommended rules
   js.configs.recommended,
+
+  // Next.js recommended rules
   nextPlugin.configs['core-web-vitals'],
+
+  // Your overrides
   {
+    plugins: {
+      "@next/next": nextPlugin
+    },
     rules: {
-      semi: ["error", "always"],
-      quotes: ["error", "single"]
+      semi: ["error", "always"],      // Require semicolons
+      quotes: ["error", "single"]     // Enforce single quotes
     }
   }
 ];
